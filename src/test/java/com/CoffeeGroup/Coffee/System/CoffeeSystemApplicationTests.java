bc6c995faf4b7998f58a48ee4cf1fd9a852be04a
Elian Rehbani <elianrhbn@gmail.com>
2026-04-24 19:31:46 -0300

    Proyecto de cero

---
+++ src/test/java/com/CoffeeGroup/Coffee/System/CoffeeSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.CoffeeGroup.Coffee.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
