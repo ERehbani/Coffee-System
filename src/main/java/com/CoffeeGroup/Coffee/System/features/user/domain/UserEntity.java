012e4f58d7dc3db6a0e6e98109847d0ac90e66f8
Elian Rehbani <=>
2026-04-27 18:39:01 -0300

    User Entity

---
+++ src/main/java/com/CoffeeGroup/Coffee/System/features/user/domain/UserEntity.java
@@ -0,0 +1,22 @@
+package com.CoffeeGroup.Coffee.System.features.user.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "external_id", nullable = false, unique = true)
+    private UUID externalId;
+}
