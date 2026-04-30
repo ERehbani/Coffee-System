3aa6415238f8b1e7f0bec1bfd760cdc1ac9a784c
Tomas Delio <tomidjk@gmail.com>
2026-04-30 10:38:01 -0300

    Relaciones desde cliente hasta beneficios
    Relacion :
    Cliente -tier
    Cliente-beneficioCompartido
    tier-beneficios
    beneficioCompartido-beneficios

--- src/main/java/com/CoffeeGroup/Coffee/System/features/shared_rewards/SharedRewardsEntity.java
+++ src/main/java/com/CoffeeGroup/Coffee/System/features/shared_rewards/SharedRewardsEntity.java
@@ -1,5 +1,7 @@
 package com.CoffeeGroup.Coffee.System.features.shared_rewards;
 
+import com.CoffeeGroup.Coffee.System.features.benefit.BenefitEntity;
+import com.CoffeeGroup.Coffee.System.features.client.ClientEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -18,12 +20,29 @@ public class SharedRewardsEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
+    private Long Reward_id;
+
+    @Column(nullable = false)
+    private java.time.LocalDate date;
+
     @Column(name = "external_id", nullable = false, unique = true)
     private UUID externalId;
 
     //RELACION CON BENEFICIO
-    //RELACION CON CLIENTE EMISOR
-    //RELACION CON CLIENTE RECEPTOR
+    @OneToOne
+    @JoinColumn(name = "benefit_id")
+    private BenefitEntity benefit;
 
+    //RELACION CON CLIENTE EMISOR
+
+    @ManyToOne
+    @JoinColumn(name = "emisor_id",nullable = false)
+    private ClientEntity emisor;
+
+    //RELACION CON CLIENTE RECEPTOR
+    @ManyToOne
+    @JoinColumn(name = "receptor_id",nullable = false)
+    private  ClientEntity receptor;
 
 }
